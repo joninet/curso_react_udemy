656860ef1f90f73ca98a633ffa61a8ff91082254
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2025-08-12 19:46:13 +0000

    ok

--- 03-gif-expert-app/src/GifExpertApp.jsx
+++ 03-gif-expert-app/src/GifExpertApp.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory'
 
 export const GifExpertApp = () =>  {
+
+    const [ categories, setCategories ] = useState(['One Punch', 'Dragon']);
+
+    const onAddCategory = () =>{
+        setCategories([...categories, 'hola'])
+        //setCategories(agregar => [...agregar, 'hola'])
+    }
+
     return(
         <>
         <h1> GifExpertApp </h1>
 
+        <AddCategory/>
 
-        
+        <button onClick={ onAddCategory }>Agregar</button>
+
+        <ol>
+            { categories.map( category => {
+                return <li key={ category }> { category } </li>
+            } ) }
+        </ol>
         </>
     )
 }
